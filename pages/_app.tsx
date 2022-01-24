9b35781c5a60e737b6c883ba64a8bbcd0a684b00
OliWalker <oliwalker@protonmail.com>
2022-01-24 11:24:38 +0100

    Adds initial boilerplate for project

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/css-reset.css";
+import "../styles/theme.css";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
